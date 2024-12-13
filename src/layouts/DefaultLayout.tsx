c426d509ac0ad7aa7317f989f1ecc2349e3851f1
Ali Nasir <alinasir@Alis-Laptop.local>
2024-12-13 19:18:51 +0330

    fix: mui rtl

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,9 +1,16 @@
 import AppBar from "@/components/layouts/AppBar";
+import { TextField } from "@mui/material";
 
 const DefaultLayout: React.FC<{children: React.ReactNode}> = ({children}) => {
     return(
         <>
             <AppBar />
+            <TextField
+                label="ملصق"
+                placeholder="العنصر النائب"
+                helperText="هذا نص مساعد"
+                variant="outlined"
+            />
             <main>
                 {children}
             </main>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
+
+// Create rtl cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [prefixer, rtlPlugin],
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CacheProvider value={cacheRtl}>
+      <App />
+    </CacheProvider>
   </StrictMode>,
 )
